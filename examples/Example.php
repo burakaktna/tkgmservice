87b2c8ff6846364f58b13862567a50fa4f1855ae
Muhammed Burak AKTUNA <burak@leafletsoft.com.tr>
2020-09-27 16:56:53 +0300

    V1.0.0 Released

--- examples/Example.php
+++ examples/Example.php
@@ -3,31 +3,65 @@
 
 namespace Burakaktna\TKGMService;
 
+use GuzzleHttp\Exception\GuzzleException;
+
+/**
+ * Class Example
+ * @package Burakaktna\TKGMService
+ */
 class Example
 {
+    /**
+     * @var TKGMService
+     */
     private TKGMService $tkgmService;
 
+    /**
+     * Example constructor.
+     * @param TKGMService $tkgmService
+     */
     public function __construct(TKGMService $tkgmService)
     {
         $this->tkgmService = $tkgmService;
     }
 
-    public function getProvinces(): array
+    /**
+     * @return object
+     * @throws GuzzleException
+     */
+    public function getProvinces(): object
     {
         return $this->tkgmService->getProvinces()->submit();
     }
 
-    public function getDistricts(int $districtId): array
+    /**
+     * @param int $districtId
+     * @return object
+     * @throws GuzzleException
+     */
+    public function getDistricts(int $districtId): object
     {
         return $this->tkgmService->getDistricts($districtId)->submit();
     }
 
-    public function getNeighborhoods(int $districtId): array
+    /**
+     * @param int $districtId
+     * @return object
+     * @throws GuzzleException
+     */
+    public function getNeighborhoods(int $districtId): object
     {
         return $this->tkgmService->getNeighborhoods($districtId)->submit();
     }
 
-    public function parcelInquiry(int $neighborhoodId, int $bobId, int $parcelId): array
+    /**
+     * @param int $neighborhoodId
+     * @param int $bobId
+     * @param int $parcelId
+     * @return object
+     * @throws GuzzleException
+     */
+    public function parcelInquiry(int $neighborhoodId, int $bobId, int $parcelId): object
     {
         return $this->tkgmService->parcelInquiry($neighborhoodId, $bobId, $parcelId)->submit();
     }
--- src/TKGMService.php
+++ src/TKGMService.php
@@ -5,12 +5,29 @@ namespace Burakaktna\TKGMService;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 
+/**
+ * Class TKGMService
+ * @package Burakaktna\TKGMService
+ */
 class TKGMService
 {
+    /**
+     * @var string
+     */
     private string $baseUrl;
+    /**
+     * @var string
+     */
     private string $url;
+    /**
+     * @var Client
+     */
     private Client $client;
 
+    /**
+     * TKGMService constructor.
+     * @param Client $client
+     */
     public function __construct(Client $client)
     {
         $this->client = $client;
@@ -18,30 +35,49 @@ class TKGMService
     }
 
 
+    /**
+     * @param string $baseUrl
+     * @return $this
+     */
     public function setBaseUrl(string $baseUrl): TKGMService
     {
         $this->baseUrl = $baseUrl;
         return $this;
     }
 
+    /**
+     * @param string $url
+     * @return $this
+     */
     public function setUrl(string $url): TKGMService
     {
         $this->url = $this->baseUrl . $url;
         return $this;
     }
 
+    /**
+     * @return $this
+     */
     public function getProvinces(): TKGMService
     {
         $this->setUrl('idariYapi/ilListe');
         return $this;
     }
 
+    /**
+     * @param int $provinceId
+     * @return $this
+     */
     public function getDistricts(int $provinceId): TKGMService
     {
         $this->setUrl('idariYapi/ilceListe/' . $provinceId);
         return $this;
     }
 
+    /**
+     * @param int $districtId
+     * @return $this
+     */
     public function getNeighborhoods(int $districtId): TKGMService
     {
         $this->setUrl('idariYapi/mahalleListe/' . $districtId);
@@ -49,6 +85,12 @@ class TKGMService
     }
 
 
+    /**
+     * @param int $neighborhoodId
+     * @param int $bobId
+     * @param int $parcelId
+     * @return $this
+     */
     public function parcelInquiry(int $neighborhoodId, int $bobId, int $parcelId): TKGMService
     {
         $this->setUrl('parsel/' . $neighborhoodId . '/' . $bobId . '/' . $parcelId);
@@ -56,10 +98,10 @@ class TKGMService
     }
 
     /**
-     * @return mixed
+     * @return object
      * @throws GuzzleException
      */
-    public function submit()
+    public function submit(): object
     {
         $response = $this->client->get($this->url);
 
